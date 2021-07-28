57b0ac28f72a348094910d79d4547784ec340aad
Francis Tseng <f+accounts@frnsys.com>
2021-07-28 15:37:00 -0700

    config editor, not totally finished

---
+++ prototype/editor/main.js
@@ -0,0 +1,5 @@
+import {createApp} from 'vue';
+import Editor from './Editor.vue'
+
+const app = createApp(Editor);
+app.mount('#main');
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: {
     'main': ['./src/main'],
     'prototype': ['./prototype/src/main'],
+    'editor': ['./prototype/editor/main'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
