f243fed5d680e1f3e818fc4f16bb8f3b5658f4b9
Francis Tseng <f+accounts@frnsys.com>
2021-08-17 19:04:21 -0400

    dynamically import Globe as its the heaviest module (due to three.js)

--- src/components/Globe.vue
+++ src/components/Globe.vue
@@ -3,12 +3,17 @@
 </template>
 
 <script>
-import Globe from '../earth/globe';
+// Dynamically import (code split) the Globe module.
+// It's the only one that uses threejs, which adds like ~300-400KB
+// to the bundle size.
+const getGlobe = () => import('../earth/globe');
 export default {
   mounted() {
-    this.globe = new Globe(this.$el);
-    this.globe.render();
-    this.globe.init();
+    getGlobe().then(({default: Globe}) => {
+      this.globe = new Globe(this.$el);
+      this.globe.render();
+      this.globe.init();
+    });
   }
 }
 </script>
