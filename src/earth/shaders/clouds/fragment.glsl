5b26effbf2b6893f656038c0a5c61f4b566a0cb6
Francis Tseng <f+accounts@frnsys.com>
2021-08-17 10:41:52 -0400

    more shader tweaks

--- src/earth/shaders/clouds/fragment.glsl
+++ src/earth/shaders/clouds/fragment.glsl
@@ -8,5 +8,5 @@ void main() {
     // Apply cloud layer
     float scale = 0.25 + sin(time/200000.)/5.;
     float n = clouds(vertexPosition + time/200000., scale);
-    gl_FragColor = n < 0.58 ? vec4(0.0) : vec4(n+0.3);
+    gl_FragColor = n < 0.58 ? vec4(0.0) : vec4(n+0.35);
 }
--- src/earth/shaders/globe/fragment.glsl
+++ src/earth/shaders/globe/fragment.glsl
@@ -8,47 +8,22 @@ uniform sampler2D satTexture;
 uniform sampler2D biomesTexture;
 uniform vec3 screenRes;
 
-/* #include "../lib/bit_dither.glsl" */
 #include "../lib/simple_dither.glsl"
-/* #include "../lib/detect_edges.glsl" */
 
 void main() {
-    // Heightmap coloring
-    // https://www.shadertoy.com/view/XsKBzt
-    /* vec3 heightColor; */
-    /* if (height > 0.5) { */
-    /*     heightColor = vec3(1. - 0.1 * clamp(floor(1.5 * height), 0., 4.)); */
-    /* } else { */
-    /*     heightColor = vec3(1.0, 1.0, 1.0); */
-    /* } */
-    /* vec3 heightEdges = detect_edges(heightColor); */
-    /* vec3 heightEdges = detect_edges(texture2D(heightmap, vertexUV).rgb); */
-
     // Color based on biomes
     vec3 color = texture2D(biomesTexture, vertexUV).rgb;
-    /* vec3 color = scale2x(biomesTexture, vertexUV).rgb; */
 
     // Add satellite texture
     color *= texture2D(satTexture, vertexUV).r * 1.5;
 
-    // Add light edge highlighting
+    // Add precomputed shadows
     vec3 shadows = texture2D(shadows, vertexUV).rgb;
-    /* vec3 edges = max(min(heightEdges, detect_edges(color)), 0.75); */
 
-    // Atmospheric glow
-    /* float intensity = 1.05 - dot(vertexNormal, vec3(0.0, 0.0, 1.0)); */
-    float intensity = 1.05 - dot(vertexNormal, vec3(0.0, 0.0, 1.0));
+    // Atmospheric glow and sphere shadow, to give more depth
+    float intensity = 1.20 - dot(vertexNormal, vec3(0.0, 0.0, 1.0));
     vec3 atmosphere = vec3(0.3, 0.3, 0.3) * pow(intensity, 3.);
     float sphereShadow = clamp(dot(vertexNormal, vec3(-2.0, 3.0, 3.0)), -0.5, 1.0);
 
-    // A kind of digital display effect?
-    // Probably too distracting
-    /* vec2 checker = step(0.5, fract(vertexUV.xy * 1000.)); */
-    /* float multiplier = (1. + (checker.x * checker.y * 0.5)); */
-    /* gl_FragColor = vec4(bit_dither((shadows * color * edges) + atmosphere), 1.0) * multiplier; */
-    /* gl_FragColor = vec4(bit_dither((shadows * color * edges) + atmosphere), 1.0); */
-    /* gl_FragColor = vec4(bit_dither((shadows * color * edges) + atmosphere * sphereShadow), 1.0); */
-    /* gl_FragColor = vec4(bit_dither((shadows * color) + atmosphere * sphereShadow) * multiplier, 1.0); */
-    /* gl_FragColor = vec4(bit_dither((shadows * color * edges) + atmosphere * sphereShadow) * multiplier, 1.0); */
     gl_FragColor = vec4(simple_dither((shadows * color) + atmosphere * sphereShadow), 1.0);
 }
