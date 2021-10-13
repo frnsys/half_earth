9e5d8fa0e62300aa588a605917b8ce753f8da99d
Francis Tseng <f+accounts@frnsys.com>
2021-10-12 20:37:53 -0400

    added missing editor image module

---
+++ editor/app/image.py
@@ -0,0 +1,25 @@
+import os
+from werkzeug import secure_filename
+from datetime import datetime, timezone
+
+UPLOAD_PATH = 'uploads'
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'JPG', 'jpeg']
+
+def allowed_file(fname):
+    return '.' in fname and fname.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def make_filename(filename):
+    filename = secure_filename(filename)
+    ext = filename.split('.')[-1]
+    timestamp = datetime.utcnow().replace(tzinfo=timezone.utc).timestamp()
+    filename = str(timestamp) + '.' + ext
+    return filename
+
+def save_image(file):
+    if allowed_file(file.filename):
+        filename = make_filename(file.filename)
+        file.save(os.path.join(UPLOAD_PATH, filename))
+        return filename
+
+def image_path(fname):
+    return os.path.join('..', UPLOAD_PATH), fname
