50cc91de127ab23dbb25ff70e637ab4c4998f409
Francis Tseng <f+accounts@frnsys.com>
2021-11-30 09:45:01 -0500

    added sentry for error monitoring, closes #19

--- src/main.js
+++ src/main.js
@@ -7,6 +7,21 @@ import tip from 'components/tip/directive';
 import App from 'components/App.vue';
 import debug from './debug';
 
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: "https://9c8cd525d7c64214836351b406f6e860@o545203.ingest.sentry.io/6087646",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
 const app = createApp(App);
 app.directive('tip', tip);
 app.config.globalProperties['consts'] = consts;
