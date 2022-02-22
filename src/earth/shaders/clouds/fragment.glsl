c7d61da8151ff5ae92660e0d94fefaf334429ec3
Francis Tseng <f+accounts@frnsys.com>
2022-02-22 11:37:07 -0800

    better clouds, fixes #131

--- src/earth/shaders/clouds/fragment.glsl
+++ src/earth/shaders/clouds/fragment.glsl
@@ -6,7 +6,7 @@ varying vec3 vertexPosition;
 
 void main() {
     // Apply cloud layer
-    float scale = 0.25 + sin(time/200000.)/5.;
-    float n = clouds(vertexPosition + time/200000., scale);
+    float scale = 0.25 + max(sin(time/10000.), 0.75)/5.;
+    float n = clouds(vertexPosition + time/10000., scale);
     gl_FragColor = n < 0.58 ? vec4(0.0) : vec4(n+0.35);
 }
