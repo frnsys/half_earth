46e265b7b425b5a3cf48211a5b4742dcce309901
Francis Tseng <f+accounts@frnsys.com>
2022-03-23 07:45:30 -0700

    added sentry monitoring to logserver

--- logserver/main.py
+++ logserver/main.py
@@ -1,6 +1,19 @@
+import config
 from db import Database
 from flask_cors import CORS
 from flask import Flask, request, jsonify
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn=config.SENTRY_DSN,
+    integrations=[FlaskIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
 
 db = Database('logs.db')
 
