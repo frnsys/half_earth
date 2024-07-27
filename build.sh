6d666a8da379d00c08cfa20ccfa304a0eea48dde
Francis Tseng <f+accounts@frnsys.com>
2024-07-27 19:07:23 +0200

    more build stuff

--- build.sh
+++ build.sh
@@ -1,55 +1,9 @@
 #!/bin/bash
 # Usage: ./build.sh <PLATFORM>, e.g. `./build.sh ITCH` or `./build.sh STEAM`
-# Build Electron apps for itch.io and steam
 # Requires https://itchio.itch.io/butler
 
 # exit when any command fails
 set -e
-
-MAINDIR=$(pwd)
-function realizeSymlinks() {
-    cd "$1/app/assets"
-    for f in $(find . -type l); do
-        SOURCE="$MAINDIR/assets/$(dirname "$f")/$(readlink "$f")"
-        echo "Copying $SOURCE to $f"
-        rm -r "$f"
-        cp -r --remove-destination "$SOURCE" "$f"
-    done
-    cd "$MAINDIR"
-    echo "Removing dirs"
-    rm -rf "$1/app/sharing"
-    rm -rf "$1/app/assets/src"
-    rm -rf "$1/app/assets/cutscenes/src"
-    rm -rf "$1/app/assets/environments/src"
-    rm -rf "$1/app/assets/backgrounds/src"
-}
-
-rm -rf ../build
-
-export SENTRY_ORG=jain-family-institute
-export SENTRY_PROJECT=half-earth
-npm run build-wasm
-export PLATFORM="$1"
-npm run build
-
-# win32 requires a clean wine prefix,
-# since mine main prefix is configured in a way that
-# is incompatible with electron-forge.
-npx electron-forge package --platform linux
-npx electron-forge package --platform darwin
-WINEPREFIX=~/work/half_earth/wine npx electron-forge package --platform win32
-
-# Have to manually replace symlinks,
-# Electron can't seem to do it on its own??
-realizeSymlinks "out/Half-Earth Socialism-darwin-x64/Half-Earth Socialism.app/Contents/Resources"
-realizeSymlinks "out/Half-Earth Socialism-linux-x64/resources"
-realizeSymlinks "out/Half-Earth Socialism-win32-x64/resources"
-
-npx electron-forge make --skip-package --platform linux
-npx electron-forge make --skip-package --platform darwin
-npx electron-forge make --skip-package --platform win32
-
-mv out ../build
 
 if [ "$1" == "ITCH" ]; then
     ../itchio/butler push "../build/make/zip/linux/x64/Half-Earth Socialism-linux-x64-1.0.0.zip" frnsys/half-earth-socialism:linux-stable
