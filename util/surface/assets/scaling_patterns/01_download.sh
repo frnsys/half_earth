e8920faaf2a384a91dbd1947d640f5dfda0dbffd
Katrin Leinweber <katrinleinweber@users.noreply.github.com>
2025-02-26 13:59:40 +0100

    refactor(util): DRY code and drop wget dependency

--- util/surface/assets/scaling_patterns/01_download.sh
+++ util/surface/assets/scaling_patterns/01_download.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-mkdir -p data/src
-cd data/src
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/tas_Amon_CanESM2_esmrcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/tas_Amon_CESM1-BGC_rcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/tas_Amon_GFDL-ESM2G_rcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/tas_Amon_MIROC-ESM_esmrcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/tas_Amon_MPI-ESM-LR_esmrcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/tas_Amon_MRI-ESM1_esmrcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/pr_Amon_CanESM2_rcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/pr_Amon_CESM1-BGC_rcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/pr_Amon_GFDL-ESM2G_rcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/pr_Amon_MIROC-ESM_rcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/pr_Amon_MPI-ESM-LR_rcp85_r1i1p1_200601-210012_pattern.rds
-wget https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps/pr_Amon_MRI-ESM1_rcp85_r1i1p1_200601-210012_pattern.rds
+
+dl_dir="data/src"
+dl_url="https://github.com/JGCRI/hectorui/raw/main/inst/shinyApp/www/maps"
+
+mkdir -p "$dl_dir"
+
+for file in \
+  pr_Amon_CanESM2_rcp85_r1i1p1_200601-210012_pattern.rds        \
+  pr_Amon_CESM1-BGC_rcp85_r1i1p1_200601-210012_pattern.rds      \
+  pr_Amon_GFDL-ESM2G_rcp85_r1i1p1_200601-210012_pattern.rds     \
+  pr_Amon_MIROC-ESM_rcp85_r1i1p1_200601-210012_pattern.rds      \
+  pr_Amon_MPI-ESM-LR_rcp85_r1i1p1_200601-210012_pattern.rds     \
+  pr_Amon_MRI-ESM1_rcp85_r1i1p1_200601-210012_pattern.rds       \
+  tas_Amon_CanESM2_esmrcp85_r1i1p1_200601-210012_pattern.rds    \
+  tas_Amon_CESM1-BGC_rcp85_r1i1p1_200601-210012_pattern.rds     \
+  tas_Amon_GFDL-ESM2G_rcp85_r1i1p1_200601-210012_pattern.rds    \
+  tas_Amon_MIROC-ESM_esmrcp85_r1i1p1_200601-210012_pattern.rds  \
+  tas_Amon_MPI-ESM-LR_esmrcp85_r1i1p1_200601-210012_pattern.rds \
+  tas_Amon_MRI-ESM1_esmrcp85_r1i1p1_200601-210012_pattern.rds
+do
+  curl --silent --output="$dl_dir"/"$file" "$dl_url"/"$file"
+done
