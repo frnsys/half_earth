73d6b1a42352e5a92297c3916b4dc2a997c3e1cd
Denperidge <denperidge@gmail.com>
2025-04-27 17:47:37 +0200

    fix: shell.nix rust target, path

--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
-
+/*
+  This file...
+  - Uses fenix to get a nightly rust toolbelt
+  - Automatically installs runtime dependencies
+  - Adds cargo to path
+*/
 { pkgs ? import <nixpkgs> { 
   overlays = [ 
     (import "${fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz"}/overlay.nix")
@@ -7,10 +12,17 @@
   pkgs.mkShell {
     nativeBuildInputs = with pkgs.buildPackages; [
       just
-      cargo-leptos
-      trunk
-      (fenix.complete.withComponents [
-        "cargo"
+      #cargo-leptos
+      #trunk
+      (with fenix; combine [
+        latest.cargo
+        latest.rustc
+        targets.wasm32-unknown-unknown.latest.rust-std
       ])
     ];
+    shellHook = ''
+      cargo install trunk
+      cargo install cargo-leptos
+    '';
+    PATH="${builtins.getEnv "PATH"}:${builtins.getEnv "HOME"}/.cargo/bin";
 }
